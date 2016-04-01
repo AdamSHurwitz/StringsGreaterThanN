7fe2ac54870e162f2f87bcbdf8968d2d3b2838e8
adamhurwitz <adamscotthurwitz@gmail.com>
2016-03-31 22:00:43 -0700

    Initial commit

---
+++ src/main/java/JavaClass.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+/**
+ * Created by adamhurwitz on 3/10/16.
+ */
+public class JavaClass {
+    static ArrayList<String> strs = new ArrayList<String>();
+    static int n = 3;
+
+    public static void main(String arg[]) {
+        strs.add("bob");
+        strs.add("cats");
+        strs.add("tires");
+        System.out.println(grtrThanN(n, strs));
+    }
+
+    public static ArrayList<String> grtrThanN(int n,  ArrayList<String> strs){
+        ArrayList<String> sol = new ArrayList<String>();
+        for (String s : strs){
+            if (s.length() > n){
+                sol.add(s);
+            }
+        }
+        return sol;
+    }
+
+
+}
